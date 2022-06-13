dfe0c274b55f2c28a0c442c2f0a8141ca4dc9415
romsh79 <48875410+romsh79@users.noreply.github.com>
2022-06-13 13:21:47 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit
 
-echo "\
-[general]\n\
-email = \"rshchelushkin@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
